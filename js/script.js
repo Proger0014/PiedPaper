dbf0e0f25d17681de151b59c0bebc629ffa31069
Proger0014 <iakov56@yandex.ru>
2021-07-03 17:19:22 +0600

    smooth scroll

---
+++ js/script.js
@@ -0,0 +1,20 @@
+document.querySelectorAll('a.nav__link').forEach(link => {
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const href = this.getAttribute('href').substring(1);
+
+        const scrollTarget = document.getElementById(href);
+
+        const topOffset = 100;
+
+        const elementPosition = scrollTarget.getBoundingClientRect().top;
+        const offsetPosition = elementPosition - topOffset;
+
+        window.scrollBy({
+            top: offsetPosition,
+            behavior: 'smooth'
+        })
+    })
+});
+
